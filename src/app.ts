21c3a9c1ab03cc693d6aac41c4f8d19eabdcd5bc
zalman0813 <zalman0813@gmail.com>
2022-11-06 18:10:37 +0800

    validate func

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,46 @@
 // Code goes here!
 
+interface Validatable { 
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable): boolean { 
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string'
+        ) { 
+        isValid =
+            isValid && validatableInput.value.length >= validatableInput.minLength
+    }
+    if (validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string'
+        ) { 
+        isValid =
+            isValid && validatableInput.value.length <= validatableInput.maxLength
+    }
+    if (validatableInput.min != null &&
+        typeof validatableInput.value === 'number'
+        ) { 
+        isValid =
+            isValid && validatableInput.value >= validatableInput.min
+    }
+    if (validatableInput.max != null &&
+        typeof validatableInput.value === 'number'
+        ) { 
+        isValid =
+            isValid && validatableInput.value <= validatableInput.max
+    }
+    return isValid
+}
+
 function Autobind(_: any, _2: string, propertyDescriptor: PropertyDescriptor) { 
     const originalMethod = propertyDescriptor.value
     const adjDescriptor: PropertyDescriptor = { 
@@ -59,10 +100,25 @@ class ProjectInput {
         const enteredTitle = this.titleInputElement.value;
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true,
+        }
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        }
+        const peopleValidatable: Validatable = {
+            value: enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        }
         if (
-            enteredTitle.trim().length == 0 ||
-            enteredDescription.trim().length == 0 ||
-            enteredPeople.trim().length == 0
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
             ) { 
             alert("Invalid input, please try again");
             return
