c9dad5832d9e751660a5312c9444eb10596d656f
zalman0813 <zalman0813@gmail.com>
2022-11-06 17:04:56 +0800

    interate with dom

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement; 
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    peopleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    titleInputElement: HTMLInputElement;
     
     constructor() { 
         this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
@@ -15,7 +18,22 @@ class ProjectInput {
             true
         );
         this.element = importNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input'
+        
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+        this.configure();
         this.attach();
+    }
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        console.log(this.titleInputElement);
+    }
+    
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
     }
 
     private attach() { 
