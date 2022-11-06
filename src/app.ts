dca234dee4e55c70d777bc01b71556598b35bdc9
zalman0813 <zalman0813@gmail.com>
2022-11-06 16:40:59 +0800

    dom element selection

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
 // Code goes here!
+
+
+class ProjectInput {
+    templateElement: HTMLTemplateElement; 
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    
+    constructor() { 
+        this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+        const importNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
+        this.element = importNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() { 
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+
+const pjInput = new ProjectInput();
